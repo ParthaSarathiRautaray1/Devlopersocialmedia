ae504e1b655f1f4efb21606c93acf14486ce6cec
ParthaSarathiRautaray1 <145197547+ParthaSarathiRautaray1@users.noreply.github.com>
2025-09-16 08:31:05 +0530

    create a express server

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+
+
+app.use("/hello",(req , res) => {
+    res.send("Hello from the Server");
+    
+})
+
+app.use("/test",(req , res) => {
+    res.send("Test from the Server");
+    
+})
+
+app.use((req , res) => {
+    res.send("Response from the HOME PAGE");
+    
+})
+
+app.listen(3000 , () =>{
+    console.log("Server is listening on port 3000");
+    
+})
